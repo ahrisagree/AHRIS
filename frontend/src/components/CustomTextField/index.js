4534ba4309f21b02b10ea8433cbb100dd89c07be
Leonardo81 <leonardoeinstein2000@gmail.com>
2021-04-05 15:55:12 +0700

    sample component material UI

---
+++ frontend/src/components/CustomTextField/index.js
@@ -0,0 +1,25 @@
+import { TextField, withStyles } from "@material-ui/core";
+
+const CustomTextField = withStyles({
+  root: {
+    '& label.Mui-focused': {
+      color: 'green',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'green',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'red',
+      },
+      '&:hover fieldset': {
+        borderColor: 'yellow',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'green',
+      },
+    },
+  },
+})(TextField);
+
+export default CustomTextField;
--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -1,8 +1,119 @@
 import React from 'react';
+import {
+  Paper,
+  TableContainer,
+  Table,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+  Container,
+  Button,
+  TextField
+} from '@material-ui/core';
+import CustomButton from 'components/CustomButton';
+import CustomTextField from 'components/CustomTextField';
 
 const Home = props => {
+  const dataTable = [
+    {
+      name: 'Frozen yoghurt',
+      calories: 159,
+      fat: 6.0,
+      carbs: 24, 
+      protein: 4.0
+    },
+    {
+      name: 'Padang',
+      calories: 159,
+      fat: 6.0,
+      carbs: 24, 
+      protein: 4.0
+    },
+    {
+      name: 'Frozen yoghurt',
+      calories: 1339,
+      fat: 6.0,
+      carbs: 24, 
+      protein: 4.0
+    },
+  ]
+
+
   return (
-    <div>Halo</div>
+    <Container component={Paper}>
+
+    {/** table */}
+      <TableContainer component={Paper}>
+        <Table className="" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {dataTable.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/** table */}
+
+      <div className="p-9">
+        <Button
+          variant="contained" 
+          style={{
+            boxShadow: "black 2px 2px",
+            margin: "1rem",
+            backgroundColor: "#FF0000"
+          }}
+          >
+          Click Button
+        </Button>
+
+        <Button
+          variant="outlined"
+          color="primary" 
+          size="small"
+          >
+          Click Button
+        </Button>
+
+        <CustomButton
+          style={{
+            margin: "1rem",
+          }}
+        >INI CUSTOM</CustomButton>
+      </div>
+
+      <div className="m-7 p-2">
+          <TextField 
+            variant="outlined"
+            style={{}}
+            size="medium"
+            label="Textfield"
+          />
+
+          <CustomTextField
+            variant="outlined"
+            label="Custom Ini"
+          />
+      </div>
+      
+    </Container>
   )
 };
 
