9ba66e299410dbc632de91c8aca7b44d797025fc
Leonardo81 <leonardoeinstein2000@gmail.com>
2021-04-11 20:53:26 +0700

    deploy3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -201,7 +201,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
