8dd1ec210dafe3ac87990390511faaacff4546e0
billhsyn <billau24@yahoo.com>
2021-04-13 22:49:56 +0700

    frontend/profil

--- frontend/src/views/Profil/Profil.js
+++ frontend/src/views/Profil/Profil.js
@@ -8,27 +8,44 @@ import {
   Typography,
   ButtonBase,
   TextField,
+  Divider,
 } from '@material-ui/core';
-import ReactDOM from "react-dom";
-
-// import "./styles.css";
 import Button  from "components/Button";
-import CustomTextField from 'components/CustomTextField';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(5),
-    marginTop: "5%",
-    boxShadow: "11px 13px 37px rgba(0, 0, 0, 0.25)",
-    margin: 'auto',
-    maxWidth: 1000,
     width : 'stretch',
-    height:  "100%",
-    minHeight: 500,
+    // marginTop: "5%",
+    padding: theme.spacing(2),
+    maxWidth: 1000,
+    margin: 'auto',
+
+
   },
+  paper1: {
+    padding: theme.spacing(5),
+    boxShadow: "11px 13px 37px rgba(0, 0, 0, 0.25)",
+    maxWidth: 1000,
+    height:  '100%',
+    minHeight: 500,
+    alignItems:"flex-end",
+    backgroundColor: '#0A3142',
+  },
+  paper2: {
+    padding: theme.spacing(5),
+    boxShadow: "11px 13px 37px rgba(0, 0, 0, 0.25)",
+    maxWidth: 1000, 
+    height:  '100%',
+    minHeight: 500,
+    },
+  profil: {
+   backgroundColor: '#0A3142',
+  },
+  profill: {
+    margin: 'auto',
+    alignItems: "left",
+   },
   image: {
     width: 128,
     height: 128,
@@ -46,109 +63,135 @@ const Profil = props => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>
-        
-        <Grid container spacing={2}>
-          <Grid item xs={3}>
-            <ButtonBase className={classes.image}>
-              <img className={classes.img} alt="complex" src="/static/images/grid/complex.jpg" />
-            </ButtonBase>
-            <Button
-                alignItems="flex-end"
-                onClick={() => {
-                  console.log("You Clicked on Me!");
-                }}
-                type="button"
-                buttonStyle="btnBlue"
-                buttonSize="btnLong"
-              >
-                Logout
-          </Button>
-          </Grid>
-
-          <Grid item xs sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  Nama
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  Set nama
-                </Typography>
-                <Typography gutterBottom variant="subtitle1">
-                  Role
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  Set role
-                </Typography>
-                <Typography gutterBottom variant="subtitle1">
-                  Divisi
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  Set divisi
-                </Typography>
-                <Typography gutterBottom variant="subtitle1">
-                  Gaji
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  Set gaji
-                </Typography>
-              </Grid>
-              {/* <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  Remove
-                </Typography>
-              </Grid> */}
+    <Grid container direction='row'>
+      <Grid item xs={12} container>
+        {/* yg warna biru */}
+        <Grid item xs={3}>
+          <Paper className={classes.paper1}>
+          <Grid container spacing={2} display='flex' justify="space-between" direction='column'>
+            <Grid item xs={12} container>
+            <Grid item xs={12}>
+            <Typography gutterBottom variant="subtitle1">
+                      Nama
+            </Typography>
+            <Typography variant="body2" color="textPrimary">
+                      Email
+                    </Typography>
             </Grid>
-            
-        <Grid item xs={6}>
-            < Grid item xs container direction="column">
-              <Typography variant="subtitle1">Change Password</Typography>
-              <TextField 
-            variant="outlined"
-            style={{}}
-            size="small"
-            label="Old Password"
-            margin = "normal"
-          />
-          <TextField 
-            variant="outlined"
-            style={{}}
-            size="small"
-            label="New Password "
-            margin = "normal"
-          />
-           <TextField 
-            variant="outlined"
-            style={{}}
-            size="small"
-            label="New Password Confirmation"
-            margin = "normal"
-          />
-          <Button
-                onClick={() => {
-                  console.log("You Clicked on Me!");
-                }}
-                type="button"
-                buttonStyle="btnBlue"
-                buttonSize="btnSmall"
-              >
-                Ubah Password
-          </Button>
-            </Grid>
+               <Grid item xs={12}>
+                  <Button
+                    onClick={() => {
+                      console.log("You Clicked on Me!");
+                    }}
+                    type="button"
+                    buttonStyle="btnBlueOutline"
+                    buttonSize="btnLong"
+                  >
+                    Logout
+                  </Button>               
+                </Grid>
             </Grid>
           </Grid>
+          </Paper>
         </Grid>
-      </Paper>
+
+        {/* yg putih */}
+        <Grid item xs={9}>
+          <Paper className={classes.paper2}>
+          <Grid container spacing={1} direction='row'>
+            <Grid item xs={12} container>
+               <Grid item xs={5}>
+                  <Typography gutterBottom variant="subtitle1">
+                      Nama
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      Set nama
+                    </Typography>
+                    <Typography gutterBottom variant="subtitle1">
+                      Role
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      Set role
+                    </Typography>
+                    <Typography gutterBottom variant="subtitle1">
+                      Divisi
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      Set divisi
+                    </Typography>
+                    <Typography gutterBottom variant="subtitle1">
+                      Gaji
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      Set gaji
+                    </Typography>
+               </Grid>
+               <Divider orientation="vertical" flexItem />
+               < Grid item xs={1}>
+               </Grid>
+               < Grid item xs={5}>
+                    <Grid container spacing={2} direction='row'>
+                      <Grid item xs={12} container>
+                        <Grid item xs={12}>
+                        <Typography variant="subtitle1">Change Password</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <TextField 
+                            variant="outlined"
+                            style={{}}
+                            size="small"
+                            label="Old Password"
+                            margin = "normal"
+                          />
+                        </Grid> 
+                        <Grid item xs={12}>
+                          <TextField 
+                            variant="outlined"
+                            style={{}}
+                            size="small"
+                            label="New Password "
+                            margin = "normal"
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <TextField 
+                            variant="outlined"
+                            style={{}}
+                            size="small"
+                            label="New Password Confirmation"
+                            margin = "normal"
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Button
+                                onClick={() => {
+                                  console.log("You Clicked on Me!");
+                                }}
+                                type="button"
+                                buttonStyle="btnBlue"
+                                buttonSize="btnSmall"
+                              >
+                                Ubah Password
+                          </Button>
+                          </Grid>
+                      </Grid>
+                </Grid>
+               </Grid>
+            </Grid>
+          </Grid>
+          </Paper>
+        </Grid>
+
+  
+      </Grid>
+    </Grid>
     </div>
-      
+
     
  
   
   );
 }
 
-// const Home = document.getElementById("root");
-// ReactDOM.render(<App />, Home);
 
 export default Profil;
