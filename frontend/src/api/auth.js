2dda9284bb087082359848458665ae5ec4de52ba
Leonardo <47312797+leleonardo81@users.noreply.github.com>
2021-04-15 02:23:30 +0700

    Revert "Stefan/frontend"

--- frontend/src/api/auth.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import { baseUrl } from './constant';
-
-
-export const loginAPI = data => axios.post(`${baseUrl}/auth/login/`, data);
-
-export const logoutAPI = () => axios.post(`${baseUrl}/auth/logout/`);
