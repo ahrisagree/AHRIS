06fc8b0bfcbd564780fb9c1dd8a572c3c58e70fb
adyssa fairuz <adyssafh@gmail.com>
2021-04-22 00:16:31 +0700

    revisi modal

--- frontend/src/components/DialogFail/index.js
+++ frontend/src/components/DialogFail/index.js
@@ -75,21 +75,10 @@ const DialogContent = withStyles(styles)((props) => {
     );
 });
 
-export default function CustomizedDialogs() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+export default function CustomizedDialogs({open, handleClose}) {
 
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open dialog
-      </Button>
       <Dialog aria-labelledby="customized-dialog-title" open={open}>
         <DialogContent dividers style={{alignItems: "center"}} onClose={handleClose}>
             <Typography style={{fontFamily: "IBM Plex Sans"}}>
