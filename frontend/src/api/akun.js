765e60a4c2c825c3179f3f88aae6303bf2e1c088
Viera Djawahir <viera@Vieras-MacBook-Air.local>
2021-04-22 15:57:06 +0700

    daftar karyawan + filters

--- frontend/src/api/akun.js
+++ frontend/src/api/akun.js
@@ -6,3 +6,13 @@ export const registerAkunAPI = data =>
     axios.post(`${baseUrl}/auth/accounts/registration/`, data);
 
 export const getDivisiAPI = () => axios.get(`${baseUrl}/divisi/`);
+
+export const getListDaftarKaryawan= query => {
+    // const queryAvailable = ['search', 'jenis', 'kategori', 'disablepagination'];
+    let formatedQuery = ""
+    Object.keys(query).forEach(qkey=>formatedQuery+=query[qkey]?`${qkey}=${query[qkey]}&`:'');
+    return axios.get(`${baseUrl}/user/?${formatedQuery}`);
+}
+
+export const getDaftarKaryawan = id =>
+    axios.get(`${baseUrl}/user/${id}/`);
