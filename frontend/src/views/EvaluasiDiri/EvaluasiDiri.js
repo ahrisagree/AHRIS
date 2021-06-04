0c29941229c04617981fae8d986d8d285757aa4e
Alyssa Kunigen Izra <Alyssa@Alyssas-MacBook-Pro.local>
2021-06-04 14:40:27 +0700

    evaluasi diri

--- frontend/src/views/EvaluasiDiri/EvaluasiDiri.js
+++ frontend/src/views/EvaluasiDiri/EvaluasiDiri.js
@@ -1,19 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   makeStyles,
   Paper,
   Container,
   Grid,
   Typography,
-  MenuItem,
 } from '@material-ui/core';
 import TextField from 'components/CustomTextField';
 import MainTitle from 'components/MainTitle';
-import Dialog from 'components/Dialog';
+// import Dialog from 'components/Dialog';
 import DialogFail from 'components/DialogFail';
 import TemplateButton from 'components/TemplateButton';
-import { buatLogAPI } from 'api/log';
-import Loading from 'components/Loading';
+import { postEvaluasiDiri } from 'api/hasilperforma';
+// import Loading from 'components/Loading';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -53,10 +52,40 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const EvaluasiDiri = props => {
+const EvaluasiDiri = ({match, history}) => {
   const classes = useStyles();
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState({});
+
+  
+  const [current_performance, setCurrent_Performance] = React.useState("");
+  const [to_do, setTo_do] = React.useState("");
+  const [parameter, setParameter] = React.useState("");
+  // const [feedback, setFeedback] = React.useState("");  // INI
+  // const [createEvaluasi, setCreateEvaluasi] = React.useState(false);
+  const [update, setUpdate] = React.useState(0);
+
+
+
+
+  const onSubmit = (
+  ) => {
+    const { id } = match.params;
+    setLoading(true);
+    postEvaluasiDiri({
+      current_performance,
+      to_do,
+      parameter,
+      // feedback,  pantes kmrn gabisa, harusnya null tp diatas diisi "" strng kosong
+      hasil_performa : id,
+    }).then(()=>history.push(`/hasil-performa/${id}`)
+    ).catch(err=>{
+      console.error(err.response);
+      setError(err.response && err.response.data);
+    }).finally(()=>{
+      setLoading(false);
+    })
+  }
 
   
     return (
@@ -66,43 +95,41 @@ const EvaluasiDiri = props => {
 
           <Grid item xs={12}>
             <Typography style={{ fontWeight: 600, marginLeft: '1%', marginBottom: '3%', fontFamily: 'IBM Plex Sans', fontStyle: 'normal', 
-            fontWeight: 600, fontSize: 24, lineHeight: '138%', display: 'flex', alignItems: 'center', letterSpacing: '0.0075em', color: '#0A3142' }} 
+            fontSize: 24, lineHeight: '138%', display: 'flex', alignItems: 'center', letterSpacing: '0.0075em', color: '#0A3142' }} 
             variant="subtitle1">
               Evaluasi Diri
             </Typography>
           </Grid>
 
-          <Grid item xs={12}>
-            <TextField id="outlined-full-width"
-              required="true"
-              label="Periode"
-              style={{ margin: 8, width: "30%" }}
-              margin="normal"
-              variant="outlined"
-              disabled={loading}
-              />
-          </Grid>
 
           <Grid item xs={12}>
             <TextField id="outlined-full-width"
               required="true"
+              value = {current_performance}
+              onChange = {e=>setCurrent_Performance(e.target.value)}
               label="Current Performance"
               style={{ margin: 8, width: "98%" }}
               margin="normal"
               variant="outlined"
+              error={!!error.current_performance}
+              helperText={error.current_performance && error.current_performance[0]}
               disabled={loading}
               />
           </Grid>
 
           <Grid item xs={12}>
             <TextField id="outlined-multiline-static"
+            value = {to_do}
+            onChange = {e=>setTo_do(e.target.value)}
             label="Action(s) to do"
             multiline
             rows={4}
             variant="outlined"
             required="true"
             style={{ margin: 8, width: "98%" }}
+            error={!!error.to_do}
             margin="normal"
+            helperText={error.to_do && error.to_do[0]}
             disabled={loading}
             />
             
@@ -111,12 +138,16 @@ const EvaluasiDiri = props => {
           <Grid item xs={12}>
             <TextField id="outlined-multiline-static"
             label="Parameter"
+            value = {parameter}
+            onChange = {e=>setParameter(e.target.value)}
             multiline
             rows={4}
             variant="outlined"
             required="true"
             style={{ margin: 8, width: "98%" }}
             margin="normal"
+            error={!!error.parameter}
+            helperText={error.parameter && error.parameter[0]}
             disabled={loading}
             />
             
@@ -124,6 +155,7 @@ const EvaluasiDiri = props => {
 
           <div className="flex justify-center py-6">
           <TemplateButton
+              onClick={onSubmit}              
               type="button"
               buttonStyle="btnBlue"
               buttonSize="btnLong"
@@ -134,8 +166,16 @@ const EvaluasiDiri = props => {
         </div>
 
         </Container>
-        <Loading open={loading} />
+        {/* <Dialog open={!!createEvaluasi} handleClose={()=>setCreateEvaluasi(false)} ></Dialog> */}
+        <DialogFail
+          open={!!error.detail} 
+          handleClose={()=>{
+            delete error.detail;
+            setUpdate(update+1);
+          }} 
+          text={error.detail}
+          />
       </div>
     )
 };
-export default EvaluasiDiri;     
+export default EvaluasiDiri; 
