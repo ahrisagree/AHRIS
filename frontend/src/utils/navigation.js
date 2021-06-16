f3214c438646475fe4024fbee18e819d482ac608
Leonardo81 <leonardoeinstein2000@gmail.com>
2021-06-16 22:13:11 +0700

    feedback FE manager

--- frontend/src/utils/navigation.js
+++ frontend/src/utils/navigation.js
@@ -7,102 +7,6 @@ import LocalAtmRoundedIcon from '@material-ui/icons/LocalAtmRounded';
 import CheckIcon from '@material-ui/icons/Check';
 import BookIcon from '@material-ui/icons/Book';
 
-// export const TEST_NAVIGATION = [
-//   {
-//     menu: 'Home',
-//     icon: <HomeIcon style={{ color: 'white' }}/>,
-//     path: '/'
-//   },
-//   {
-//     menu: 'Kelola Akun',
-//     icon: <PeopleAltIcon style={{ color: 'white' }}/>,
-//     children: [
-//       {
-//         menu: 'List Akun',
-//         path: '/akun',
-//       },
-//       {
-//         menu: 'Buat Akun',
-//         path: '/akun/register',
-//       }
-//     ]
-//   },
-//   {
-//     menu: 'Presensi',
-//     icon: <CheckIcon style={{ color: 'white' }}/>,
-//     children: [
-//       {
-//         menu: 'Buat Presensi',
-//         path: '/',
-//       },
-//       {
-//         menu: 'Presensi Saya',
-//         path: '/my-presensi',
-//       }
-//     ]
-//   },
-//   {
-//     menu: 'Daily Log',
-//     icon: <BookIcon style={{ color: 'white' }}/>,
-//     children: [
-//       {
-//         menu: 'List Daily Log',
-//         path: '/log',
-//       },
-//       {
-//         menu: 'Buat Log',
-//         path: '/log-aktivitas',
-//         icon: <AddRounded />
-//       }
-//     ]
-//   },
-//   {
-//     menu: 'Paket Borang',
-//     icon: <InsertDriveFileOutlinedIcon style={{ color: 'white' }}/>,
-//     children: [
-//       {
-//         menu: 'List Borang',
-//         path: '/paket-pertanyaan'
-//       },
-//       {
-//         menu: 'Buat Borang',
-//         path: '/paket-pertanyaan/add',
-//       },
-//       {
-//         menu: 'Assign Borang',
-//         path: '/assign'
-//       }
-//     ]
-//   },
-//   {
-//     menu: 'Performa',
-//     icon: <InsertDriveFileOutlinedIcon style={{ color: 'white' }}/>,
-//     children: [
-//       {
-//         menu: 'Kelola Hasil Performa',
-//         path: '/kelola-performa'
-//       },
-//       {
-//         menu: 'Isi Borang',
-//         path: '/mengisi-borang'
-//       },
-//       {
-//         menu: 'Evaluasi Performa',
-//         path: '/daftar-evaluasi-performa'
-//       },
-//       {
-//         menu: 'Hasil Performa',
-//         path: '/hasil-performa'
-//       },
-//     ]
-//   },
-//   {
-//     menu: 'Gaji',
-//     path: '/gaji',
-//     icon: <LocalAtmRoundedIcon style={{ color: 'white' }}/>,
-//   },
-// ]
-
 
 export const KARYAWAN_NAVIGATION = [
   {
@@ -129,7 +33,7 @@ export const KARYAWAN_NAVIGATION = [
     icon: <BookIcon style={{ color: 'white' }}/>,
     children: [
       {
-        menu: 'List Daily Log',
+        menu: 'My Daily Log',
         path: '/log',
       },
       {
@@ -175,35 +79,6 @@ export const ADMIN_NAVIGATION = [
       }
     ]
   },
-  // {
-  //   menu: 'Presensi',
-  //   icon: <CheckIcon style={{ color: 'white' }}/>,
-  //   children: [
-  //     {
-  //       menu: 'Buat Presensi',
-  //       path: '/',
-  //     },
-  //     {
-  //       menu: 'Presensi Saya',
-  //       path: '/my-presensi',
-  //     }
-  //   ]
-  // },
-  // {
-  //   menu: 'Daily Log',
-  //   icon: <BookIcon style={{ color: 'white' }}/>,
-  //   children: [
-  //     {
-  //       menu: 'List Daily Log',
-  //       path: '/log',
-  //     },
-  //     {
-  //       menu: 'Buat Log',
-  //       path: '/log-aktivitas',
-  //       icon: <AddRounded />
-  //     }
-  //   ]
-  // },
   {
     menu: 'Paket Borang',
     icon: <InsertDriveFileOutlinedIcon style={{ color: 'white' }}/>,
@@ -272,8 +147,12 @@ export const MANAGER_NAVIGATION = [
     icon: <BookIcon style={{ color: 'white' }}/>,
     children: [
       {
-        menu: 'List Daily Log',
+        menu: 'My Daily Log',
         path: '/log',
+      },
+      {
+        menu: 'Daftar Log Karayawan',
+        path: '/log/daftar-log-karyawan'
       },
       {
         menu: 'Buat Log',
@@ -285,12 +164,7 @@ export const MANAGER_NAVIGATION = [
   {
     menu: 'Paket Borang',
     icon: <InsertDriveFileOutlinedIcon style={{ color: 'white' }}/>,
-    children: [
-      {
-        menu: 'List Borang',
-        path: '/paket-pertanyaan'
-      }
-    ]
+    path: '/paket-pertanyaan'
   },
   {
     menu: 'Performa',
